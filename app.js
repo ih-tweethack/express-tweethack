6ca4b512c0af8a047ba469770eede61f81c9bba6
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2023-02-06 19:04:41 +0100

    prepare express setup

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const express = require('express');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+/* DB connection */
+require('./config/db.config');
+
+const app = express();
+
+app.use(logger('dev')); // logger de morgan para ver las peticiones que se hacen
+app.use(express.urlencoded({ extended: false })); // para que el body de las peticiones se pueda leer
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+/** Configure static files */
+app.use(express.static('public'));
+
+/* Routes */
+
+const router = require('./config/routes.config');
+app.use('/', router)
+
+/* Errors middlewares */
+
+app.use((req, res, next) => {
+  next(createError(404, 'Resource not found'));
+});
+
+app.use((error, req, res, next) => {
+  console.log(error)
+  let status = error.status || 500;
+
+  res.status(status).render('error', {
+    message: error.message,
+    error: req.app.get('env') === 'development' ? error : {}
+  })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
---
+++ config/db.config.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', true)
+
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/express-tweethack'
+
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  autoIndex: true, //make this also true
+})
+  .then(response => console.log(`Connected to Mongo! Database name: '${response.connections[0].name}'`))
+  .catch(err => console.error('Error connecting to mongo', err))
+
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected on app termination');
+    process.exit(0);
+  });
+});
