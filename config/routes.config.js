ccac95b6b9236341a1911380e024618287ce427c
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2023-02-06 21:20:41 +0100

    feat(#4): auth middleware and routes protection

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,20 +1,21 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 /* Main route */
 router.get('/', (req, res, next) => res.send('Hello world'))
 
 /* Auth */
-router.get('/signup', authController.signup);
-router.post('/signup', authController.doSignup);
+router.get('/signup', authMiddleware.isNotAuthenticated, authController.signup);
+router.post('/signup', authMiddleware.isNotAuthenticated, authController.doSignup);
 
-router.get('/login', authController.login);
-router.post('/login', authController.doLogin);
+router.get('/login', authMiddleware.isNotAuthenticated, authController.login);
+router.post('/login', authMiddleware.isNotAuthenticated, authController.doLogin);
 
-router.get('/logout', authController.doLogout);
+router.get('/logout', authMiddleware.isAuthenticated, authController.doLogout);
 
 /* User */
-router.get('/timeline', userController.timeline);
+router.get('/timeline', authMiddleware.isAuthenticated, userController.timeline);
 
 module.exports = router;
